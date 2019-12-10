a8d545e02a6c62490c133a1babf77accea639f62
Ntronik5000 <noeljimenez.400@gmail.com>
2019-12-10 09:51:36 -0400

    no message

--- PointOfSalesV2/PointOfSalesV2.Api/Security/ActionAuthorize.cs
+++ PointOfSalesV2/PointOfSalesV2.Api/Security/ActionAuthorize.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Caching.Memory;
+using PointOfSalesV2.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,10 @@ namespace PointOfSalesV2.Api.Security
 {
     public class ActionAuthorize : TypeFilterAttribute
     {
-        public ActionAuthorize() : base(typeof(ActionRequirementFilter))
+        public ActionAuthorize(int actionId) : base(typeof(ActionRequirementFilter))
         {
 
-
+            Arguments = new object[] { actionId };
         }
 
 
@@ -24,13 +25,13 @@ namespace PointOfSalesV2.Api.Security
 
     public class ActionRequirementFilter : IAuthorizationFilter
     {
-
+        readonly int _action;
         readonly IHttpContextAccessor _httpContextAccessor;
         readonly IMemoryCache _cache;
 
-        public ActionRequirementFilter(IHttpContextAccessor httpContextAccessor, IMemoryCache cache)
+        public ActionRequirementFilter(int action, IHttpContextAccessor httpContextAccessor, IMemoryCache cache)
         {
-
+            _action = action;
             _httpContextAccessor = httpContextAccessor;
             _cache = cache;
         }
@@ -41,16 +42,25 @@ namespace PointOfSalesV2.Api.Security
             var currentToken = _httpContextAccessor.HttpContext.Request.Headers.FirstOrDefault(x => x.Key == "Authorization").Value.ToString();
 
 
-            if (string.IsNullOrEmpty(currentToken))
+            var currentPath = _httpContextAccessor.HttpContext.Request.Path.ToString().Split("/").ToList();
+            int index = currentPath.IndexOf("api") + 1;
+            string currentController = currentPath[index];
+            if (string.IsNullOrEmpty(currentToken) || string.IsNullOrEmpty(currentController) || !currentToken.Contains("Bearer"))
                 isInvalid = true;
 
             if (!isInvalid)
             {
-                string token = currentToken;
-                object user = _cache.Get<object>(token);
+                string token = currentToken.Split(" ").LastOrDefault();
+                User user = _cache.Get<User>(token);
                 if (user == null)
                     context.Result = new ForbidResult();
+                else
+                {
+                    //var operations = user.UserRoles.Where(r => r.Role.Controllers != null && r.Role.Controllers.Exists(c => c.ControllerName.ToLower() == currentController.ToLower())).Select(x => x.Role.Controllers).SelectMany(a => a.SelectMany(b => b.Actions)).ToList();
 
+                    //if (user.UserRoles?.Count > 0 && !operations.Exists(o => o == _action))
+                    //    context.Result = new ForbidResult();
+                }
 
             }
             else
--- PointOfSalesV2/PointOfSalesV2.Entities/User.cs
+++ PointOfSalesV2/PointOfSalesV2.Entities/User.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace PointOfSalesV2.Entities
 {
@@ -14,18 +15,75 @@ namespace PointOfSalesV2.Entities
         public override long Id { get => base.Id; set => base.Id = value; }
         [Key]
         public Guid UserId { get; set; }
-        [MaxLength(50)]
-        public string Name { get; set; }
-        [MaxLength(50)]
-        public string LastName { get; set; }
-        [MaxLength(50)]
-        [EmailAddress(ErrorMessage = "Invalid Email Address")]
-        [Key]
-        public string Email { get; set; }
-        [MaxLength(500)]
-        public string Password { get; set; }
+       
 
         [NotMapped]
         public string TokenKey { get; set; }
+
+        [MaxLength(30)]
+        [DataMember]
+        [Required]
+        public string Name { get; set; }
+
+
+        [MaxLength(30)]
+        [DataMember]
+        [Required]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public DateTime? BirthDay { get; set; }
+
+        [DataMember]
+        public short GenereId { get; set; }
+
+        [DataMember]
+        [MaxLength(15)]
+        public string Phone { get; set; }
+
+        [MaxLength(15)]
+        [DataMember]
+        public string Movil { get; set; }
+
+        [MaxLength(200)]
+        [DataMember]
+        public string Address { get; set; }
+
+
+        [MaxLength(30)]
+        [DataMember]
+        [Required]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [DataMember]
+        [Required]
+        public string Password { get; set; }
+
+        [MaxLength(150)]
+        [DataType(DataType.EmailAddress)]
+        [DataMember]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        public byte[] ImageData { get; set; }
+        [MaxLength(15)]
+        public string ContentType { get; set; }
+        public double? Width { get; set; }
+        public double? Height { get; set; }
+
+        public double? Size { get; set; }
+
+
+
+
+        [NotMapped]
+        public string FullName => Name + "  " + LastName;
+
+        public virtual char Gender { get; set; }
+
+       
+
+      
     }
 }
